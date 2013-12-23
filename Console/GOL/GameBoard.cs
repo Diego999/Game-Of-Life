0c8fea37ecd64db204b613c38d15e6e52ae09cda
Diego Antognini <diegoantognini@gmail.com>
2013-12-23 12:41:44 +0100

    First basic version of the game of life with console

---
+++ Console/GOL/GameBoard.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOL
+{
+    class GameBoard
+    {
+        public enum State {Alive, Emerging, Dying, Dead};
+        private static readonly Dictionary<State, string> STATE_MATCH;
+
+        private int x;
+        private int y;
+        private State[,] gameBoard;
+
+        static GameBoard()
+        {
+            STATE_MATCH = new Dictionary<State, string>();
+            STATE_MATCH.Add(State.Alive, "X");
+            STATE_MATCH.Add(State.Dead, " ");
+        }
+
+        public GameBoard(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            gameBoard = new State[x, y];
+            for (int i = 0; i <= gameBoard.GetUpperBound(0); ++i)
+                for (int j = 0; j <= gameBoard.GetUpperBound(1); ++j)
+                    gameBoard[i, j] = State.Dead;
+            gameBoard[12, 15] = gameBoard[12, 14] = gameBoard[12, 16] = gameBoard[11, 16] = gameBoard[10, 15] = State.Alive;
+        }
+
+        public State this[int k1, int k2]
+        {
+            get
+            {
+                manageKeys(ref k1, ref k2);
+                return gameBoard[k1, k2];
+            }
+            set
+            {
+                manageKeys(ref k1, ref k2);
+                gameBoard[k1, k2] = value;
+            }
+        }
+
+        public int GetUpperBound(int dim)
+        {
+            return gameBoard.GetUpperBound(dim);
+        }
+
+        private void manageKeys(ref int k1, ref int k2)
+        {
+            if (k1 < 0)
+                k1 += x;
+            if (k2 < 0)
+                k2 += y;
+            if (k1 >= x)
+                k1 -= x;
+            if (k2 >= y)
+                k2 -= y;
+        }
+
+        public void draw()
+        {
+            for (int j = 0; j <= gameBoard.GetUpperBound(1)+2; ++j)
+                Console.Write("-");
+            Console.WriteLine();
+            for (int i = 0; i <= gameBoard.GetUpperBound(0); ++i)
+            {
+                Console.Write("|");
+                for (int j = 0; j <= gameBoard.GetUpperBound(1); ++j)
+                {
+                    Console.Write(STATE_MATCH[gameBoard[i, j]]);
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            for (int j = 0; j <= gameBoard.GetUpperBound(1)+2; ++j)
+                Console.Write("-");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Console/GOL/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOL
+{
+    
+    class Program
+    {
+        private const int X = 30;
+        private const int Y = 30;
+
+        static void Main(string[] args)
+        {
+            GameBoard gameBoard1 = new GameBoard(X, Y);
+            GameBoard gameBoard2 = new GameBoard(X, Y);
+
+            while (true)
+            {
+                gameBoard1.draw();
+                for (int i = 0; i <= gameBoard1.GetUpperBound(0); ++i)
+                    for (int j = 0; j <= gameBoard1.GetUpperBound(1); ++j)
+                    {
+                        int neighbors = 0;
+                        for (int ii = -1; ii <= 1; ++ii)
+                            for (int jj = -1; jj <= 1; ++jj)
+                                if(gameBoard1[i+ii, j+jj] == GameBoard.State.Alive)
+                                    ++neighbors;
+                        if (gameBoard1[i, j] == GameBoard.State.Alive)
+                            --neighbors;
+
+                        if (gameBoard1[i, j] == GameBoard.State.Alive && (neighbors < 2 || neighbors > 3))
+                            gameBoard2[i, j] = GameBoard.State.Dead;
+                        else if (gameBoard1[i, j] == GameBoard.State.Dead && neighbors == 3 || gameBoard1[i, j] == GameBoard.State.Alive && (neighbors == 2 || neighbors == 3))
+                            gameBoard2[i, j] = GameBoard.State.Alive;
+                        else
+                            gameBoard2[i, j] = GameBoard.State.Dead;
+                    }
+                GameBoard temp = gameBoard1;
+                gameBoard1 = gameBoard2;
+                gameBoard2 = temp;
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+
+
+        }
+    }
+}
