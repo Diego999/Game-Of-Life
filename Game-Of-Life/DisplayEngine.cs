2d6932f1ee17e26cc13a3c07ec683ad38f521e5b
Diego Antognini <diegoantognini@gmail.com>
2013-12-25 21:34:41 +0100

    Change text info and tool tip

--- Game-Of-Life/DisplayEngine.cs
+++ Game-Of-Life/DisplayEngine.cs
@@ -103,14 +103,16 @@ namespace Game_Of_Life
         /// </summary>
         public static void ShowAbout()
         {
-            MessageBox.Show(@"The game of life is a game created by John Horton Conway in 1970. The game consists in a grid of cells where a cell can have one state among several and then, they live depending 3 rules (Wikipedia) :
+            MessageBox.Show(@"The game of life is a cellular automaton created by John Horton Conway in 1970. The simulation consists in a grid of cells where a cell can have one state among several and then, they live depending 4 rules (Wikipedia) :
 
 1) Any live cell with fewer than two live neighbours dies, as if caused by under-population.
 2) Any live cell with two or three live neighbours lives on to the next generation.
 3) Any live cell with more than three live neighbours dies, as if by overcrowding.
 4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-The game generate each step of living and you can see new cells emerging, dying, dead or alive.", "About Game Of Life", MessageBoxButton.OK, MessageBoxImage.Information);
+Then the simulation will generate and display each state, from the first generation until whenever you want.
+
+You can interact with the simulation, by pressing the left button of the mouse or the right one. The left will create a cell and the right delete the selected cell. You can also mouse the move during the process.", "About Game Of Life", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         #region DisplayEngine Draw
